05ab925ac208fa77bdfdafa4a87a5de394628058
Ludvig Krantzen <ludvig.ludde23@gmail.com>
2022-10-24 13:55:11 +0200

    Initial Commit

---
+++ Lab01/Lab01App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab01App.swift
+//  Lab01
+//
+//  Created by Ludvig Krantzén on 2022-10-24.
+//
+
+import SwiftUI
+
+@main
+struct Lab01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
